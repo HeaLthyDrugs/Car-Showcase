a02673a5da9abef05d4a72bb5786a96891b2ed30
Manish Vishwakarma <hbxmanish@gmail.com>
2023-08-24 23:10:02 +0530

    basic customization and theme changed

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,13 @@ export default async function Home({ searchParams }: HomeProps) {
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
-    <main className='overflow-hidden'>
+    <main className='overflow-hidden text-teal-200' style={{ backgroundColor: "#1A1A1D" }}>
       <Hero />
 
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
+          <h1 className='text-4xl text-gray-300 font-extrabold'>Car Catalogue</h1>
+          <p className="text-gray-600">Explore out cars you might like</p>
         </div>
 
         <div className='home__filters'>
@@ -48,7 +48,7 @@ export default async function Home({ searchParams }: HomeProps) {
           </section>
         ) : (
           <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <h2 className='text-white text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+        Explore Cars, Specs, and Rent Your Ride
         </h1>
 
         <p className="hero__subtitle">
@@ -27,13 +27,13 @@ const Hero = () => {
 
         <CustomButton
           title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          containerStyles="bg-purple-400 text-black font-semibold rounded-full mt-10"
           handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/lambo.png" alt="hero" fill className="object-contain" />
         </div>
 
         <div className="hero__image-overlay" />
